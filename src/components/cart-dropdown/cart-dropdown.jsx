a61a776797c4637f2b451be9d4db99305c0e4286
tomas <tommasini15@gmail.com>
2020-09-15 10:52:11 +0100

    updating with reselect memoization
    Now we save some render, that is super important!

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
+import {selectCartItems} from '../../redux/cart/cart.selectors';
+
 import CustomButtom from '../custom-bottom/custom-button';
 import CartItem from '../cart-item/cart-item';
 import './cart-dropdown.scss';
@@ -16,8 +18,8 @@ const CartDropdown = ({cartItems}) => (
     <CustomButtom>GO TO CHECKOUT</CustomButtom>
   </div>
 )
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  cartItems
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
 });
 
 

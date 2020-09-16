87a972db20aa70e396ccf0aaea1dc4242d7ce86c
tomas <tommasini15@gmail.com>
2020-09-16 09:58:37 +0100

    Move our directory and shop data to reducer and create the selectores and updating the respective components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './redux/store';
+import {store, persistor} from "./redux/store";
 
-import './index.css';
-import App from './App';
+
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+      <PersistGate persistor= {persistor}>
+        <App />
+        </PersistGate>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,28 +1,18 @@
 import React from "react";
+import {Route} from 'react-router-dom';
 
-import CollectionPreview from'../../components/collection-preview/collection-preview.jsx';
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import CollectionPage from '../collection/collection';
 
-import SHOP_DATA from './shop.data.jsx';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
 
-  render() {
-    const {collections} = this.state;
-    return <div className='shop-page'>
-      {
-        collections.map(({id, ...otherCollectionProps}) => (
-          <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-        ))
-      }
-    </div>
-  }
-}
 
 export default ShopPage;
